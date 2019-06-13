c1c3914a42c9049aa5dc2fdaef8166b239f4bd05
mathewtrivett <matt.trivett@gmail.com>
2019-06-13 19:10:18 +0100

    Refactored the apps package

--- opencommunity/settings/base.py
+++ opencommunity/settings/base.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'api',
-    'users'
+    'opencommunity.apps.api',
+    'opencommunity.apps.users'
 ]
 
 MIDDLEWARE = [

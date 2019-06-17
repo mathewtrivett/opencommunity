dff4510738fbe68716ed1396771dcaeca1f2d45f
mathewtrivett <matt.trivett@gmail.com>
2019-06-17 19:46:13 +0100

    Added custom user model to the Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username','first_name','last_name','email','is_staff','is_active','last_login',)
+    search_fields = ('username','email',)
+
+admin.site.register(User,UserAdmin)
